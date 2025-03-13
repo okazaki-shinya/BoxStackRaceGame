330b756574efeb25dae5c834d1be457a6ec96d3e
h4ru9898 <kss-24130027@nsgcl.jp>
2025-03-13 14:42:46 +0900

    スタートマネージャー兼UIの作成

---
+++ Assets/Start/StartManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartManager : MonoBehaviour
+{
+    [SerializeField] private bool isGameStarted = false;
+    [SerializeField] private TextMeshProUGUI textUI;
+    [SerializeField] private string newText = "Game Start";
+    [SerializeField] private float displayTime = 3f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(!isGameStarted)
+            {
+                isGameStarted = true;
+                StartGame();
+            }
+        }
+    }
+
+    void StartGame()
+    {
+        Debug.Log("ゲームスタート");
+
+        textUI.text = newText;
+
+        StartCoroutine(HideUI(displayTime));
+    }
+
+    IEnumerator HideUI(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        textUI.gameObject.SetActive(false);
+    }
+}
